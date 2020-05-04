326c432d15db287b1c5b6486438545eddb317a3b
damlois <lois0168@gmail.com>
2020-05-04 21:41:31 +0100

    added code on home page to fetch posts from db

--- SelahSeries/Controllers/BlogController.cs
+++ SelahSeries/Controllers/BlogController.cs
@@ -2,8 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
+using SelahSeries.Models.DTOs;
 using SelahSeries.Repository;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -14,18 +17,32 @@ namespace SelahSeries.Controllers
     public class BlogController : Controller
     {
         private readonly ICommentRepository _commentRepo;
-        public BlogController(ICommentRepository commentRepo)
+        private readonly IMapper _mapper;
+        private readonly IHostingEnvironment hostingEnvironment;
+        private readonly IPostRepository _postRepo;
+        public BlogController(IPostRepository postRepo, IMapper mapper, IHostingEnvironment environment, ICommentRepository commentRepo)
         {
+            _postRepo = postRepo;
+            _mapper = mapper;
+            hostingEnvironment = environment;
             _commentRepo = commentRepo;
         }
         // GET: /<controller>/
-        public IActionResult Category()
+        public async Task<IActionResult> Category()
         {
-            return View();
+            var pageParam = new PaginationParam
+            {
+                PageIndex = 1,
+                Limit = 20,
+                SortColoumn = "CreatedAt"
+            };
+            var posts = await _postRepo.GetPublishedPosts(pageParam);
+            return View(posts.Source);
         }
 
         public IActionResult Post()
         {
+           
             return View();
         }
         
@@ -34,7 +51,6 @@ namespace SelahSeries.Controllers
             var comment = await _commentRepo.GetComments(postId);
             return Json(JsonConvert.SerializeObject(comment));
         }
-
 
     }
 }
